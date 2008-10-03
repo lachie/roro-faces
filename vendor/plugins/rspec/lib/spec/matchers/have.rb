21ad3dbf55b1a736a754f7d927e53a932afb9586
Xavier Shay <xavier@rhnh.net>
2008-10-02 17:27:07 -0700

    Not sure what the correct check for inflector is here but this seems to work for this app

--- vendor/plugins/rspec/lib/spec/matchers/have.rb
+++ vendor/plugins/rspec/lib/spec/matchers/have.rb
@@ -17,7 +17,7 @@ module Spec
     
       def method_missing(sym, *args, &block)
         @collection_name = sym
-        @plural_collection_name = ActiveSupport::Inflector.pluralize(sym.to_s) if Object.const_defined?(:"ActiveSupport::Inflector")
+        @plural_collection_name = ActiveSupport::Inflector.pluralize(sym.to_s) if Object.const_defined?(:Inflector)
         @args = args
         @block = block
         self
