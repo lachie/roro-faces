67d9110a7f0324dcaac81cdc05185feb686da756
lachie <lachiec@gmail.com>
2009-06-07 17:06:08 +1000

    generalising comments

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,7 @@
 module UserHelper
   def user_link(user)
-    link_to user.name_with_fallback, user_url(user)
+    if user
+      link_to user.name_with_fallback, user_url(user)
+    end
   end
 end
